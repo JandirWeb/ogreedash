5a904b4088a36a32cb18e1676e63e9296d83c796
Jandir Moreira <jandirweb@gmail.com>
2023-01-06 15:00:50 -0300

    estrutura menu principal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 import '../style/styles.scss'
+
+/* Menu Principal */
+
+let menuItem = document.querySelector('.alunos');
+
+menuItem.addEventListener('click', ()=> {menuItem.classList.toggle("active");});
