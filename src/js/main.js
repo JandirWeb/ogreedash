aa52b065f098fc565252dae6fab381164d647211
Jandir Moreira <jandirweb@gmail.com>
2023-01-06 17:25:32 -0300

    menu lateral

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,17 @@ import '../style/styles.scss'
 
 /* Menu Principal */
 
-let menuItem = document.querySelector('.alunos');
+let menuAlunos = document.querySelector('.alunos');
+menuAlunos.addEventListener('click', ()=> {menuAlunos.classList.toggle("active");});
 
-menuItem.addEventListener('click', ()=> {menuItem.classList.toggle("active");});
+let menuTurmas = document.querySelector('.turmas');
+menuTurmas.addEventListener('click', ()=> {menuTurmas.classList.toggle("active");});
+
+let menuPlanos = document.querySelector('.planos');
+menuPlanos.addEventListener('click', ()=> {menuPlanos.classList.toggle("active");});
+
+let menuFinanceiro = document.querySelector('.financeiro');
+menuFinanceiro.addEventListener('click', ()=> {menuFinanceiro.classList.toggle("active");});
+
+let menuUsuarios = document.querySelector('.usuarios');
+menuUsuarios.addEventListener('click', ()=> {menuUsuarios.classList.toggle("active");});
