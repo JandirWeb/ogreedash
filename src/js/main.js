3eea0e3ce669e9e2d514b6787e08df0c28a1c0eb
Jandir Moreira <jandirweb@gmail.com>
2023-01-07 20:31:47 -0300

    menus desk

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,52 @@
 import '../style/styles.scss'
 
-/* Menu Principal */
+/* Main Menu */
 
-let menuAlunos = document.querySelector('.alunos');
+const menuAlunos = document.querySelector('.alunos');
 menuAlunos.addEventListener('click', ()=> {menuAlunos.classList.toggle("active");});
 
-let menuTurmas = document.querySelector('.turmas');
+const menuTurmas = document.querySelector('.turmas');
 menuTurmas.addEventListener('click', ()=> {menuTurmas.classList.toggle("active");});
 
-let menuPlanos = document.querySelector('.planos');
+const menuPlanos = document.querySelector('.planos');
 menuPlanos.addEventListener('click', ()=> {menuPlanos.classList.toggle("active");});
 
-let menuFinanceiro = document.querySelector('.financeiro');
+const menuFinanceiro = document.querySelector('.financeiro');
 menuFinanceiro.addEventListener('click', ()=> {menuFinanceiro.classList.toggle("active");});
 
-let menuUsuarios = document.querySelector('.usuarios');
+const menuUsuarios = document.querySelector('.usuarios');
 menuUsuarios.addEventListener('click', ()=> {menuUsuarios.classList.toggle("active");});
+
+/* Alert Itens */
+const alertIcon = document.querySelector('.alerts');
+const alertList = document.querySelector('.alert-list');
+
+/* Submenu profile */
+const btnProfile = document.querySelector('.img-admin');
+const submenuProfile = document.querySelector('.submenu-profile');
+
+document.addEventListener('mousedown', (event) => {
+    if (alertIcon.contains(event.target)) {
+        alertList.style.display = 'block';
+        setTimeout(() => {
+            alertList.style.opacity = 1;
+          }, "200")
+    } else {
+        alertList.style.opacity = 0;
+        setTimeout(() =>{
+            alertList.style.display = 'none';
+        }, "500")
+    }
+
+    if (btnProfile.contains(event.target)) {
+        submenuProfile.style.display = 'flex';
+        setTimeout(() => {
+            submenuProfile.style.opacity = 1;
+          }, "200")
+    } else {
+        submenuProfile.style.opacity = 0;
+        setTimeout(() =>{
+            submenuProfile.style.display = 'none';
+        }, "500")
+    }
+});
